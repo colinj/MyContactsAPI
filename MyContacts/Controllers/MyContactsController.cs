80e9bbc2864535aff1acf11282c32e7716e1d004
Colin Johnsun <colin.johnsun@gmail.com>
2016-10-19 22:04:05 +1100

    Add controller

---
+++ MyContacts/Controllers/MyContactsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MyContacts.Models;
+
+namespace MyContacts.Controllers
+{
+    public class MyContactsController : ApiController
+    {
+        private ContactsService contactService;
+
+        public MyContactsController()
+        {
+            contactService = new ContactsService();
+        }
+
+        public IEnumerable<Contact> Get()
+        {
+            return contactService.GetAll();
+        }
+
+        public Contact Get(int id)
+        {
+            var contact = contactService.GetAll()
+                                        .Where(c => c.Id == id)
+                                        .SingleOrDefault();
+
+            if (contact == null)
+            {
+                var resp = new HttpResponseMessage(HttpStatusCode.NotFound)
+                {
+                    Content = new StringContent($"No contact with Id = {id}"),
+                    ReasonPhrase = "Contact Not Found"
+                };
+
+                throw new HttpResponseException(resp);
+            }
+
+            return contact;
+        }
+    }
+}
