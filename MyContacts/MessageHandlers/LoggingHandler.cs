3946884858ea09c952bd82bb815ad3a9ef6d9bfd
Colin Johnsun <colin.johnsun@gmail.com>
2016-10-25 01:20:40 +1100

    Change logging.

--- MyContacts/MessageHandlers/LoggingHandler.cs
+++ MyContacts/MessageHandlers/LoggingHandler.cs
@@ -12,25 +12,16 @@ namespace MyContacts.MessageHandlers
 {
     public class LoggingHandler : DelegatingHandler
     {
-        private const string LogPath = @"C:\Temp\";
-        private readonly string _logFile;
-
-        public LoggingHandler()
-        {
-            Directory.CreateDirectory(LogPath);
-            _logFile = LogPath + "log.txt";
-        }
-
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             var correlationId = $"{DateTime.Now.Ticks}{Thread.CurrentThread.ManagedThreadId}";
             var requestInfo = $"{request.Method} {request.RequestUri}";
-            File.AppendAllText(_logFile, $"{correlationId} - Request: {requestInfo}\r\n");
+            Trace.TraceInformation($"{correlationId} - Request: {requestInfo}\r\n");
 
             var response = await base.SendAsync(request, cancellationToken);
 
             var responseInfo = $"{(int)response.StatusCode} {response.ReasonPhrase}";
-            File.AppendAllText(_logFile, $"{correlationId} - Response: {requestInfo} -> {responseInfo}\r\n\r\n");
+            Trace.TraceInformation($"{correlationId} - Response: {requestInfo} -> {responseInfo}\r\n\r\n");
 
             return response;
         }
